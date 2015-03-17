fd8a599219ed8c39ebc8df6153eeb4717677dd3b
wnalle <nalle@hawaii.edu>
2015-03-16 16:22:15 -1000

    E 42 Attempt 2 Complete.
    Total time: 20 minutes

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,7 +1,9 @@
 package controllers;
 
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.formData.ContactFormData;
 import views.html.Index;
 import views.html.NewContact;
 
@@ -19,12 +21,24 @@ public class Application extends Controller {
   }
 
   /**
-   * Returns page1, a simple example of a second page to illustrate navigation.
-   * @return The NewContact.
+   * Returns a blank new contact page.
+   * @return The New Contact.
    */
   public static Result newContact() {
-    return ok(NewContact.render("Welcome to the New Contact page."));
+    return ok(NewContact.render(Form.form(ContactFormData.class)));
 
   }
 
+  /**
+   * Pulls the information from the new contact form and prints it to the console.
+   * @return A blank new contact form.
+   */
+  public static Result postContact() {
+    Form<ContactFormData> formData = Form.form(ContactFormData.class).bindFromRequest();
+    ContactFormData data = formData.get();
+
+    System.out.println("Received POST: " + data.getFirstName() + " " + data.getLastName() + " " + data.getTelephone());
+
+    return ok(NewContact.render(Form.form(ContactFormData.class)));
+  }
 }
---
+++ app/views/formData/ContactFormData.java
@@ -0,0 +1,40 @@
+package views.formData;
+
+/**
+ * Contact form class for the new contact page.
+ */
+public class ContactFormData {
+  /** The first name. */
+  public String firstName = "";
+  /** The last name. */
+  public String lastName = "";
+  /** The phone number. */
+  public String telephone = "";
+
+  /**
+   * Get the first name.
+   * @return The first name.
+   */
+  public String getFirstName() {
+    return firstName;
+  }
+
+  /**
+   * Get the last name.
+   * @return The last name.
+   */
+  public String getLastName() {
+    return lastName;
+  }
+
+  /**
+   * Get the telephone number.
+   * @return The telephone number.
+   */
+  public String getTelephone() {
+    return telephone;
+  }
+
+
+
+}
